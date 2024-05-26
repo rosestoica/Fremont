6cb6555713f76844059d3801bcf120da52d85243
rosestoica <rose.stoica@student.upt.ro>
2024-05-26 13:19:26 +0300

    spring_boot

---
+++ src/main/java/org/example/Fremont/FremontApplication.java
@@ -0,0 +1,13 @@
+package org.example.Fremont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FremontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FremontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/Fremont/FremontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.Fremont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FremontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
